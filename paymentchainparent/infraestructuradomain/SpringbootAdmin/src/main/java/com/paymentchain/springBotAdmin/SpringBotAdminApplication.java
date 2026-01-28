1fa8353463e6663612010c5adac8a9af30a9633f
Thiago Castoldi <castoldithiago4@gmail.com>
2026-01-28 14:17:37 -0300

    apigateway service

--- paymentchainparent/infraestructuradomain/SpringbootAdmin/src/main/java/com/paymentchain/springBotAdmin/SpringBotAdminApplication.java
+++ paymentchainparent/infraestructuradomain/SpringbootAdmin/src/main/java/com/paymentchain/springBotAdmin/SpringBotAdminApplication.java
@@ -2,14 +2,12 @@ package com.paymentchain.springBotAdmin;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableDiscoveryClient
-@EnableAutoConfiguration
 @EnableAdminServer
 @SpringBootApplication
 public class SpringBotAdminApplication {
